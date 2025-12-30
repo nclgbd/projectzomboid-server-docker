51c32dae38cb22e66eefa8883eb8695377a927b8
teddy <nguob@uw.edu>
2025-12-30 04:43:44 +0000

    Added 'latest' duplicate

--- scripts/server-backup.sh
+++ scripts/server-backup.sh
@@ -1,3 +1,5 @@
 timestamp=$(date +"%y/%m/%d")
 aws s3 cp server-data s3://pzserver-ratcord/backups/$timestamp/server-data --recursive
 aws s3 cp server-files s3://pzserver-ratcord/backups/$timestamp/server-files --recursive
+aws s3 sync s3://pzserver-ratcord/backups/$timestamp/server-data s3://pzserver-ratcord/backups/latest/server-data
+aws s3 sync s3://pzserver-ratcord/backups/$timestamp/server-files s3://pzserver-ratcord/backups/latest/server-files
