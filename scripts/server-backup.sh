67057d2635dbe89728e83fbb461b1f74f53f7be5
teddy <nguob@uw.edu>
2026-09-16 06:50:47 +0000

    Added server name specification

--- scripts/server-backup.sh
+++ scripts/server-backup.sh
@@ -1,5 +1,7 @@
 timestamp=$(date +"%y/%m/%d")
-aws s3 cp server-data s3://pzserver-ratcord/backups/$timestamp/server-data --recursive
-aws s3 cp server-files s3://pzserver-ratcord/backups/$timestamp/server-files --recursive
-aws s3 sync s3://pzserver-ratcord/backups/$timestamp/server-data s3://pzserver-ratcord/backups/latest/server-data
-aws s3 sync s3://pzserver-ratcord/backups/$timestamp/server-files s3://pzserver-ratcord/backups/latest/server-files
+SERVER_NAME=pzserver-pokemulti
+aws s3 mb s3://$SERVER_NAME
+aws s3 cp server-data s3://$SERVER_NAME/backups/$timestamp/server-data --recursive
+aws s3 cp server-files s3://$SERVER_NAME/backups/$timestamp/server-files --recursive
+aws s3 sync s3://$SERVER_NAME/backups/$timestamp/server-data s3://$SERVER_NAME/backups/latest/server-data
+aws s3 sync s3://$SERVER_NAME/backups/$timestamp/server-files s3://$SERVER_NAME/backups/latest/server-files
